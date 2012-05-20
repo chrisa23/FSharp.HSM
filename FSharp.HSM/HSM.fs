ad727024f974b424c56190d20dcb5fc693738600
ChrisA23 <ChrisA23@gmail.com>
2012-05-20 01:49:08 -0500

    Renamed, updated readme

--- FSharp.HSM/HSM.fs
+++ FSharp.HSM/HSM.fs
@@ -16,42 +16,50 @@ type StateConfig<'state,'event> =
     Entry: unit -> unit
     Exit: unit -> unit
     Transitions: Transition<'state,'event> list}
-    member this.hasSuper = match this.SuperState with
-                           | None -> false
-                           | Some _ -> true
-    member this.getSuper = match this.SuperState with
-                           | None -> Unchecked.defaultof<'state>
-                           | Some x -> x
+    member this.hasSuper = 
+      match this.SuperState with
+      | None -> false
+      | Some _ -> true
+    member this.getSuper = 
+      match this.SuperState with
+      | None -> Unchecked.defaultof<'state>
+      | Some x -> x
+
 exception NoTransition
 exception AlreadyStarted
 
 type StateMachine<'state,'event when 'state : equality and 'event :equality>(states:StateConfig<'state,'event> list ) = 
+    //below is dangerous for enums/structs I believe...
     let mutable current = Unchecked.defaultof<'state>
     let find state = states |> List.find (fun x -> x.State = state)
     let rec findTransition event state = 
-        match state.Transitions |> List.tryFind (fun x -> x.Event = event) with
-        | None -> match state.SuperState with //do we have a superstate?
-                  | None -> raise NoTransition//error
-                  | Some x -> findTransition event (find x)
-        | Some x ->   x
+      match state.Transitions |> List.tryFind (fun x -> x.Event = event) with
+      | None -> 
+        match state.SuperState with //do we have a superstate?
+        | None -> raise NoTransition//error
+        | Some x -> findTransition event (find x)
+      | Some x -> x
     let stateEvent = new Event<'state>()
+    let rec getParents list state =
+      let config = find state
+      if config.hasSuper then 
+        let super = config.getSuper
+        getParents (super::list) super
+      else list
+    let rec exitSuper state limit = 
+      match state.SuperState with
+      | None -> ()
+      | Some super -> 
+        if limit <> Unchecked.defaultof<'state> && super = limit then ()
+        else 
+          let superConfig = find super
+          superConfig.Exit()
+          exitSuper superConfig limit 
     let rec transition currentState newState = 
       let currentStateConfig = find currentState
       let newStateConfig = states |> List.find (fun x -> x.State = newState)
-
       let isSelf = currentStateConfig.State = newStateConfig.State
-      
-      let curIsSub = currentStateConfig.hasSuper
-
-      let rec getParents list state =
-        let config = find state
-        if config.hasSuper then 
-          let super = config.getSuper
-          getParents (super::list) super
-        else list
-
       //has common parent (which becomes limit on exit super)
-      //get parents in order.  first of lists to match.
       let curParents = getParents [] currentState |> List.rev
       let newParents = getParents [] newState |> List.rev
       let commonParent = ref Unchecked.defaultof<'state>
@@ -63,27 +71,15 @@ type StateMachine<'state,'event when 'state : equality and 'event :equality>(sta
           foundParent <- true//done...  STOP
 
       let moveToSub = newParents |> List.exists (fun x -> x = currentState)
-
       let shareSuper = !commonParent <> Unchecked.defaultof<'state>
-
-      let rec exitSuper state limit = 
-        match state.SuperState with
-        | None -> ()
-        | Some super -> 
-          if limit <> Unchecked.defaultof<'state> && super = limit then ()
-          else 
-            let superConfig = find super
-            superConfig.Exit()
-            exitSuper superConfig limit 
 //EXITS
       if (not moveToSub && not isSelf) || isSelf then
         currentStateConfig.Exit()
-
       //exit super states up to but not common parent
       exitSuper currentStateConfig !commonParent      
-
 //ENTRY
-//enter parents below common Parents before newState, but not if we just autoed from there..
+      //enter parents below common Parents before newState, 
+      //but not if we just autoed from there..
       if foundParent then
         let revParents = newParents |> List.rev 
         let index = revParents |> List.findIndex (fun x -> x = !commonParent)
@@ -98,7 +94,7 @@ type StateMachine<'state,'event when 'state : equality and 'event :equality>(sta
       match newStateConfig.AutoTransition with
       | None -> ()
       | Some x -> transition newState x
-
+    ///Initializes the state machine with its initial state
     member this.Init state = 
         if not (current = Unchecked.defaultof<'state>) then raise AlreadyStarted
         current <- state
@@ -113,7 +109,7 @@ type StateMachine<'state,'event when 'state : equality and 'event :equality>(sta
     member this.IsIn (state:'state) = 
       if current = state then true else
         let cur = find current
-        cur.hasSuper && cur.getSuper = state
+        getParents [] current |> List.exists (fun x -> x = state)
     member this.Fire(event) = 
         let cur = find current
         let trans = findTransition event cur
@@ -124,24 +120,31 @@ type StateMachine<'state,'event when 'state : equality and 'event :equality>(sta
         if trans.Guard() then transition current (trans.NextState event data)
 
 let configure state = 
-  { State = state; SuperState = None; Entry = (fun _ -> ()); Exit = (fun _ -> ()); AutoTransition = None; Transitions = [] }
+  { State = state; SuperState = None; 
+    Entry = (fun _ -> ()); Exit = (fun _ -> ()); 
+    AutoTransition = None; Transitions = [] }
+
+let onEntry f state = {state with Entry = f }
+let onExit f state = {state with Exit = f }
 
 let substateOf superState state = { state with SuperState = Some(superState) }
-let onEntry (f: unit->unit) state = {state with Entry = f }
-let onExit f state = {state with Exit = f }
+
+///Sets an auto transition to a new state
 let transitionTo substate state = { state with AutoTransition = Some(substate) }
 
+///Sets a transition to a new state on an event (same state allows re-entry)
 let on event endState state =
-  { state with Transitions = { Event = event; NextState =(fun x y -> endState); Guard = (fun _ -> true) }::state.Transitions }
+  { state with Transitions = { Event = event; NextState = (fun _ _ -> endState); Guard = (fun _ -> true) }::state.Transitions }
+///Sets a guarded transition to a new state on an event (same state allows re-entry)
 let onIf event guard endState state = 
-  { state with Transitions = { Event = event; NextState = (fun x y -> endState); Guard = guard }::state.Transitions }
-
+  { state with Transitions = { Event = event; NextState = (fun _ _ -> endState); Guard = guard }::state.Transitions }
+///Sets an event handler (with or without data) which returns the new state
 let handle event f state = 
   { state with Transitions = { Event = event; NextState = f; Guard = (fun _ -> true) }::state.Transitions }
+///Sets a guarded event handler (with or without data) which returns the new state
 let handleIf event guard f state = 
   { state with Transitions = { Event = event; NextState = f; Guard = guard }::state.Transitions }
 
-//todo:
-//ignore (? is this worthwhile?)
-//unhandled handler
-//implement action on transition with/wo guard
+//todo
+//let action event f state =
+//let actionIf event guard f state =
--- README.md
+++ README.md
@@ -4,3 +4,66 @@ FSharp.HSM
 Hierarchical State Machine library in F#
 
 Inspired by stateless (http://code.google.com/p/stateless/), http://accu.org/index.php/journals/252
+
+Examples:
+
+Phone call:
+    new StateMachine<State,Trigger>(
+        [ configure OffHook
+            |> on CallDialed Ringing
+          configure Ringing
+            |> on CallConnected Connected
+          configure Connected
+            |> onEntry (fun _ -> startTimer())
+            |> onExit (fun _ -> stopTimer())
+            |> transitionTo InCall
+            |> on HungUp OffHook
+          configure InCall
+            |> substateOf Connected
+            |> on PlacedOnHold OnHold
+          configure OnHold
+            |> substateOf Connected
+            |> on TakenOffHold InCall ] )
+
+Complex HSM:
+    new StateMachine<State,Sig>(
+        [ configure S0
+            |> onEntry (fun _ -> printfn "Enter S0")
+            |> onExit (fun _ -> printfn "Exit S0")
+            |> transitionTo S1
+            |> on E S211
+          configure S1
+            |> onEntry (fun _ -> printfn "Enter S1")
+            |> onExit (fun _ -> printfn "Exit S1")
+            |> substateOf S0
+            |> transitionTo S11
+            |> on A S1 
+            |> on B S11
+            |> on C S211 
+            |> on D S0 
+            |> on F S211 
+          configure S11
+            |> onEntry (fun _ -> printfn "Enter S11")
+            |> onExit (fun _ -> printfn "Exit S11")
+            |> substateOf S1
+            |> on G S211 
+			|> actionIf h (fun _ -> foo) (fun _ -> foo <- false) //still todo
+          configure S2
+            |> onEntry (fun _ -> printfn "Enter S2")
+            |> onExit (fun _ -> printfn "Exit S2")
+            |> substateOf S0
+            |> on C S1 
+            |> on F S11 
+          configure S21
+            |> onEntry (fun _ -> printfn "Enter S21")
+            |> onExit (fun _ -> printfn "Exit S21")
+            |> substateOf S2
+            |> transitionTo S211
+            |> on B S211 
+            |> handleIf H (fun _ -> not foo) (fun x y -> foo <- true; S21 )
+          configure S211
+            |> onEntry (fun _ -> printfn "Enter S211")
+            |> onExit (fun _ -> printfn "Exit S211")
+            |> substateOf S21
+            |> on D S21
+            |> on G S0 ] ) 
